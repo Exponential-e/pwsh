30d75550577b4e5d0485393df0ad6740a12a8fb4
John Burton <john.burton@exponential-e.com>
2020-07-20 17:38:38 +0100

    Create Disable-Offload.ps1
    Script to disable offload settings in Windows

---
+++ Disable-Offload.ps1
@@ -0,0 +1,20 @@
+# ========================================================================
+# Name      = Disable-Offload.ps1
+# Author    = John Burton
+# Company   = Exponential-e Ltd
+# Purpose   = Disable Offload Settings
+# Date      = 20/07/2020
+# Updated   = 20/07/2020
+# Version   = 1.0
+# ========================================================================
+
+Get-NetAdapterAdvancedProperty Ethernet
+
+Set-NetAdapterAdvancedProperty -DisplayName "IPv4 Checksum Offload" -DisplayValue "Disabled"
+Set-NetAdapterAdvancedProperty -DisplayName "IPv4 TSO Offload" -DisplayValue "Disabled"
+Set-NetAdapterAdvancedProperty -DisplayName "Large Send offload v2 (ipv4)" -DisplayValue "Disabled"
+Set-NetAdapterAdvancedProperty -DisplayName "Large Send offload v2 (ipv6)" -DisplayValue "Disabled"
+Set-NetAdapterAdvancedProperty -DisplayName "TCP checksum offload (ipv4)" -DisplayValue "Disabled"
+Set-NetAdapterAdvancedProperty -DisplayName "TCP checksum offload (ipv6)" -DisplayValue "Disabled"
+
+Get-NetAdapterAdvancedProperty Ethernet
